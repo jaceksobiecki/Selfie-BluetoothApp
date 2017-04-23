2782ed85e3d80db39d03c0864047ca75115bd1fc
Jacek Sobiecki <jsobiecki01@gmail.com>
2017-04-23 15:32:02 +0200

    adds chart

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,63 +1,133 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Slider;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
+
     private static double speed;
     @FXML
-    Slider slider;
-
-    HC05 hc05 = new HC05();
+    public AreaChart<?, ?> chart;
+    private XYChart.Series series;
+    @FXML
+    private Slider slider;
+    @FXML
+    private CategoryAxis x;
+    @FXML
+    private NumberAxis y;
+    private HC05 hc05 = new HC05();
 
     @FXML
     private void stop(ActionEvent event) throws Exception {
         hc05.send(250);
     }
+
     @FXML
     private void up(ActionEvent event) throws Exception {
         hc05.send(251);
     }
+
     @FXML
     private void down(ActionEvent event) throws Exception {
         hc05.send(252);
     }
+
     @FXML
     private void left(ActionEvent event) throws Exception {
         hc05.send(253);
     }
+
     @FXML
     private void right(ActionEvent event) throws Exception {
         hc05.send(254);
     }
+
     @FXML
     private void connect(ActionEvent event) throws IOException {
         hc05.go();
     }
+
     @FXML
     public void getData() throws IOException {
         Thread thread1 = new Thread(hc05);
         thread1.start();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb){
+    @FXML
+    public void showTestChart() {
+        hc05.drawTestData();
+        series = new XYChart.Series();
+        chart.getData().addAll(series);
 
+        Thread thread2 = new Thread(new TestChartThread());
+        thread2.start();
+    }
+
+    @FXML
+    public void showChart(){
+        series = new XYChart.Series();
+        chart.getData().addAll(series);
+
+        Thread thread2 = new Thread(new ChartThread());
+        thread2.start();
+    }
+
+    public class ChartThread implements Runnable{
+
+        @Override
+        public void run() {
+            while (true) {
+                Platform.runLater(() -> {
+                    series.getData().add(new XYChart.Data(hc05.getTimeS(), hc05.getData()));
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public class TestChartThread implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                Platform.runLater(() -> {
+                    series.getData().add(new XYChart.Data(hc05.getTimeS(), hc05.getData()));
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
         slider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable,
                                 Number oldValue, Number newValue) {
-                speed = (double)newValue;
+                speed = (double) newValue;
                 try {
-                    hc05.send((int)speed);
+                    hc05.send((int) speed);
                 } catch (Exception e1) {
                     e1.printStackTrace();
                 }
--- src/sample/HC05.java
+++ src/sample/HC05.java
@@ -3,6 +3,7 @@ package sample;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import java.io.*;
+import java.util.Random;
 
 /**
  * Connects to HC-05 bluetooth module
@@ -13,15 +14,27 @@ public class HC05 implements Runnable {
     private static InputStream is;
     private static long time3;
     private static int i=0;
-    private static int tablica1[] = new int[100];
-    private static long tablica2[] = new long[100];
+    private static int data[] = new int[100];
+    private static long time[] = new long[100];
+    private static String timeS[] = new String[100];
+    private int a =-1;
+    private int b = -1;
 
     //set your hc05Url
-    private static String hc05Url = "btspp://201611226383:1;authenticate=false;encrypt=false;master=false";
+
+    //Bt Piotrek
+    //private static String hc05Url = "btspp://201611226383:1;authenticate=false;encrypt=false;master=false";
     //bt Jacek
-    //"btspp://301412260760:1;authenticate=false;encrypt=false;master=false";
-    //bt Piotrek
-    //"btspp://201611226383:1;authenticate=false;encrypt=false;master=false";
+    private static String hc05Url = "btspp://301412260760:1;authenticate=false;encrypt=false;master=false";
+
+    public String getTimeS(){
+        a++;
+        return timeS[a];
+    }
+    public int getData(){
+        b++;
+        return data[b];
+    }
 
     public void go() throws IOException {
         streamConnection = (StreamConnection) Connector.open(hc05Url);
@@ -54,7 +67,7 @@ public class HC05 implements Runnable {
         System.out.println(reade);
 
         //zapisanie wartosci w tablicy -zestawic w tabeli
-        tablica1[i] = reade;
+        data[i] = reade;
 
         //czas zakonczenia pobierania jednej wartosci
         long time2 = System.currentTimeMillis();
@@ -64,10 +77,19 @@ public class HC05 implements Runnable {
         System.out.println(time3);
 
         //zapisanie czasu w tablicy -zestawic w tabeli
-        tablica2[i] = time3;
+        time[i] = time3;
+        timeS[i] = String.valueOf(time[i]);
         time3 += time3;
 
         i++;
+    }
+
+    public void drawTestData(){
+        for(int i=0; i<100; i++){
+            data[i] = new Random().nextInt(200);
+            time[i] = i;
+            timeS[i] = String.valueOf(time[i]);
+        }
     }
 
     @Override
