4fcdc9e451c7896ea7de40b11bd0db046296993b
szubertpiotrek <piotr.szubert1@wp.pl>
2017-11-19 01:16:41 +0100

    new receiving and sending data~from STM and Jatson

--- src/com/selfie/app/HC05.java
+++ src/com/selfie/app/HC05.java
@@ -57,7 +57,7 @@ public class HC05 {
         return devices;
     }
 
-    public  ArrayList<Short> getData() {
+    public ArrayList<Short> getData() {
         return rData;
     }
 
@@ -83,21 +83,21 @@ public class HC05 {
         os.write(a);
     }
 
-    public void send(byte[] sData1,byte[]sFlag) throws Exception {
-        j_bufferS[0] = (byte) ((sFlag[0])                           & 0xFF);
-        j_bufferS[1] = (byte) ((sData1[0])                          & 0xFF);
-        j_bufferS[2] = (byte) ((sData1[0]>>8 |sData1[1]<<3 )        & 0xFF);
-        j_bufferS[3] = (byte) ((sData1[1]>>5 |sData1[2]<<6)         & 0xFF);
-        j_bufferS[4] = (byte) ((sData1[2]>>2)                       & 0xFF);
-        j_bufferS[5] = (byte) ((sData1[3]<<1 |sData1[2]>>10)        & 0xFF);
-        j_bufferS[6] = (byte) ((sData1[4]<<4 |sData1[3]>>7)         & 0xFF);
-        j_bufferS[7] = (byte) ((sData1[5]<<7 |sData1[4]>>4)         & 0xFF);
-        j_bufferS[8] = (byte) ((sData1[5]>>1)                       & 0xFF);
-        j_bufferS[9] = (byte) ((sData1[6]<<2 |sData1[5]>>9)         & 0xFF);
-        j_bufferS[10] = (byte) ((sData1[7]<<5 |sData1[6]>>6)        & 0xFF);
-        j_bufferS[11] = (byte) ((sData1[7]>>3)                      & 0xFF);
-        j_bufferS[12] = (byte) ((sFlag[1])                          & 0xFF);
-        j_bufferS[13] = (byte) ((sFlag[2])                          & 0xFF);
+    public void send(byte[] sData1, byte[] sFlag) throws Exception {
+        j_bufferS[0] = (byte) ((sFlag[0]) & 0xFF);
+        j_bufferS[1] = (byte) ((sData1[0]) & 0xFF);
+        j_bufferS[2] = (byte) ((sData1[0] >> 8 | sData1[1] << 3) & 0xFF);
+        j_bufferS[3] = (byte) ((sData1[1] >> 5 | sData1[2] << 6) & 0xFF);
+        j_bufferS[4] = (byte) ((sData1[2] >> 2) & 0xFF);
+        j_bufferS[5] = (byte) ((sData1[3] << 1 | sData1[2] >> 10) & 0xFF);
+        j_bufferS[6] = (byte) ((sData1[4] << 4 | sData1[3] >> 7) & 0xFF);
+        j_bufferS[7] = (byte) ((sData1[5] << 7 | sData1[4] >> 4) & 0xFF);
+        j_bufferS[8] = (byte) ((sData1[5] >> 1) & 0xFF);
+        j_bufferS[9] = (byte) ((sData1[6] << 2 | sData1[5] >> 9) & 0xFF);
+        j_bufferS[10] = (byte) ((sData1[7] << 5 | sData1[6] >> 6) & 0xFF);
+        j_bufferS[11] = (byte) ((sData1[7] >> 3) & 0xFF);
+        j_bufferS[12] = (byte) ((sFlag[1]) & 0xFF);
+        j_bufferS[13] = (byte) ((sFlag[2]) & 0xFF);
         os.write(j_bufferS, 0, 14);
 
     }
@@ -105,31 +105,47 @@ public class HC05 {
     public void receiveData() throws IOException {
         DataInputStream disReader = new DataInputStream(is);
         if (is.available() > 0) {
-            rFlag[0] =disReader.readByte();
+            rFlag[0] = disReader.readByte();
             if (rFlag[0] == 0xFF) {
-                syncByte = 0xFE;
-                for (int i = 0; i < 11; i++) {
-                    j_bufferR[i] = disReader.readByte();
-                }
+                    syncByte = 0xFE;
+                    for (int i = 0; i < 11; i++) {
+                        j_bufferR[i] = disReader.readByte();
+                    }
+                    rData.add((short) ((j_bufferR[0] | j_bufferR[1] << 8) & 0x7FF));
+                    rData.add((short) ((j_bufferR[1] >> 3 | j_bufferR[2] << 5) & 0x7FF));
+                    rData.add((short) ((j_bufferR[2] >> 6 | j_bufferR[3] << 2 | j_bufferR[4] << 10) & 0x7FF));
+                    rData.add((short) ((j_bufferR[4] >> 1 | j_bufferR[5] << 7) & 0x7FF));
+                    rData.add((short) ((j_bufferR[5] >> 4 | j_bufferR[6] << 4) & 0x7FF));
+                    rData.add((short) ((j_bufferR[6] >> 7 | j_bufferR[7] << 1 | j_bufferR[8] << 9) & 0x7FF));
+                    rData.add((short) ((j_bufferR[8] >> 2 | j_bufferR[9] << 6) & 0x7FF));
+                    rData.add((short) ((j_bufferR[9] >> 5 | j_bufferR[10] << 3) & 0x7FF));
+
+                    syncByte = 0xFD;
+                    rFlag[1] = disReader.readByte();
+                    rFlag[2] = disReader.readByte();
+
+                    System.out.println(rData);
             }
-            else if(syncByte==0xFE) {
-                rData.add((short) ((j_bufferR[0] | j_bufferR[1] << 8) & 0x7FF));
-                rData.add((short) ((j_bufferR[1] >> 3 | j_bufferR[2] << 5) & 0x7FF));
-                rData.add((short) ((j_bufferR[2] >> 6 | j_bufferR[3] << 2 | j_bufferR[4] << 10) & 0x7FF));
-                rData.add((short) ((j_bufferR[4] >> 1 | j_bufferR[5] << 7) & 0x7FF));
-                rData.add((short) ((j_bufferR[5] >> 4 | j_bufferR[6] << 4) & 0x7FF));
-                rData.add((short) ((j_bufferR[6] >> 7 | j_bufferR[7] << 1 | j_bufferR[8] << 9) & 0x7FF));
-                rData.add((short) ((j_bufferR[8] >> 2 | j_bufferR[9] << 6) & 0x7FF));
-                rData.add((short) ((j_bufferR[9] >> 5 | j_bufferR[10] << 3) & 0x7FF));
+            else if (rFlag[0] == 0xFF) {
+                    syncByte = 0xFE;
+                    for (int i = 0; i < 11; i++) {
+                        j_bufferR[i] = disReader.readByte();
+                    }
+                    rData.add((short) ((j_bufferR[0] | j_bufferR[1] << 8) & 0x7FF));
+                    rData.add((short) ((j_bufferR[1] >> 3 | j_bufferR[2] << 5) & 0x7FF));
+                    rData.add((short) ((j_bufferR[2] >> 6 | j_bufferR[3] << 2 | j_bufferR[4] << 10) & 0x7FF));
+                    rData.add((short) ((j_bufferR[4] >> 1 | j_bufferR[5] << 7) & 0x7FF));
+                    rData.add((short) ((j_bufferR[5] >> 4 | j_bufferR[6] << 4) & 0x7FF));
+                    rData.add((short) ((j_bufferR[6] >> 7 | j_bufferR[7] << 1 | j_bufferR[8] << 9) & 0x7FF));
+                    rData.add((short) ((j_bufferR[8] >> 2 | j_bufferR[9] << 6) & 0x7FF));
+                    rData.add((short) ((j_bufferR[9] >> 5 | j_bufferR[10] << 3) & 0x7FF));
 
-                syncByte = 0xFD;
-                rFlag[1] = disReader.readByte();
-                rFlag[2] = disReader.readByte();
+                    syncByte = 0xFD;
+                    rFlag[1] = disReader.readByte();
+                    rFlag[2] = disReader.readByte();
 
-                System.out.println(rData);
-
+                    System.out.println(rData);
             }
-
         }
         /*
         if(data.get(0)==100)
